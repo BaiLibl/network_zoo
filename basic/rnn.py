3e268bde46c8d8920ec7280c7be11f74959bca35
BaiLibl <baili615@163.com>
2021-10-13 19:22:19 +0800

    Update rnn.py

--- basic/rnn.py
+++ basic/rnn.py
@@ -1,3 +1,64 @@
-# LSTM
-# GRU
-# text
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.modules.rnn import LSTM
+import torch.optim as optim
+
+# POS by LSTM
+
+# datasets
+def prepare_sequence(seq, to_ix):
+    idxs = [to_ix[w] for w in seq]
+    return torch.tensor(idxs, dtype=torch.long)
+
+training_data = [
+    ("The dog ate the apple".split(), ["DET", "NN", "V", "DET", "NN"]),
+    ("Everybody read that book".split(), ["NN", "V", "DET", "NN"])
+]
+word_to_ix = {}
+for sent, tags in training_data:
+    # print(sent, tags)
+    for word in sent:
+        if word not in word_to_ix:  # word has not been assigned an index yet
+            word_to_ix[word] = len(word_to_ix)  # Assign each word with a unique index
+# print(word_to_ix)
+tag_to_ix = {"DET": 0, "NN": 1, "V": 2}  # Assign each tag with a unique index
+
+
+# network
+EMBEDDING_DIM = 6
+HIDDEN_DIM = 6
+
+class LSTMTagger(nn.Module):
+    def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size):
+        super(LSTMTagger, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+        self.output = nn.Linear(hidden_dim, tagset_size)
+
+    def forward(self, sentence):
+        embeds = self.word_embeddings(sentence)
+        lstm_out, _ = self.lstm(embeds.view(len(sentence), 1, -1))
+        tag_space = self.output(lstm_out.view(len(sentence), -1))
+        tag_scores = F.log_softmax(tag_space, dim=1)
+        return tag_scores
+
+model = LSTM(EMBEDDING_DIM, HIDDEN_DIM, len(word_to_ix), len(tag_to_ix))
+loss_function = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.1)
+
+for epoch in range(1): 
+    for sentence, tags in training_data:
+        model.zero_grad()
+        sentence_in = prepare_sequence(sentence, word_to_ix)
+        targets = prepare_sequence(tags, tag_to_ix)
+        tag_scores = model(sentence_in)
+        loss = loss_function(tag_scores, targets)
+        loss.backward()
+        optimizer.step()
+
+# with torch.no_grad():
+#     inputs = prepare_sequence(training_data[0][0], word_to_ix)
+#     tag_scores = model(inputs)
+#     print(tag_scores)
